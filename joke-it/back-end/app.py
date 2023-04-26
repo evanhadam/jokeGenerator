fa38efd878c72bdb36997929fcc779387df11686
Jonathan Wukitsch <11484046+jonathanwuki@users.noreply.github.com>
2023-04-25 22:34:44 -0400

    Support bucket and quick sort

--- joke-it/back-end/app.py
+++ joke-it/back-end/app.py
@@ -1,10 +1,13 @@
+import math
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from joke_bucketsort import perform_bucketsort
+from joke_quicksort import perform_quicksort
 
 app = Flask(__name__)
 CORS(app)
  
-@app.route('/test', methods =['POST'])
+@app.route('/test', methods=['POST'])
 def test():
    profanityMin = request.json['sliderMin1']
    profanityMax = request.json['sliderMax1']
@@ -13,8 +16,22 @@ def test():
    lengthMin = request.json['sliderMin3']
    lengthMax = request.json['sliderMax3']
 
-   # need json file of 10 titles and jokes, along with the times taken for quick and bucket sort
-   return jsonify({"Result": "Welcome to GeeksForGeeks" +request.json['sliderMin1'] + request.json['sliderMax1'] + request.json['sliderMin2'] + request.json['sliderMax2'] + request.json['sliderMin3'] + request.json['sliderMax3']})
- 
+   # 48526 is the best joke
+   max_pop_n = math.floor(48526 * (int(popularityMax) / 100))
+   min_pop_n = math.floor(48526 * (int(popularityMin) / 100))
+   max_prof_n = int(profanityMax)
+   min_prof_n = int(profanityMin)
+   max_length_n = math.floor(10000 * (int(lengthMax) / 100))
+   min_length_n = math.floor(10000 * (int(lengthMin) / 100))
+   
+   # print(min_length_n)
+   # print(max_length_n)
+   # print(min_pop_n)
+   # print(max_pop_n)
+   # print(min_prof_n)
+   # print(max_prof_n)
+
+   return perform_bucketsort(10, min_length_n, max_length_n, min_pop_n, max_pop_n, min_prof_n, max_prof_n)
+
 if __name__ == '__main__':
     app.run(debug = True)
