99fa92831585b9442191b324e92c74251725c747
Jonathan Wukitsch <jwukitsch6@gmail.com>
2023-04-27 12:37:04 -0400

    Add comments

--- joke-it/back-end/joke_bucketsort.py
+++ joke-it/back-end/joke_bucketsort.py
@@ -9,6 +9,8 @@ json_array = json.load(input_file)
 
 jokes = []
 
+# Performs insertion sort, as used in the bucket sort algorithm per GeeksForGeeks:
+# https://www.geeksforgeeks.org/bucket-sort-2
 def insertion_sort(bucket):
     for i in range(1, len(bucket)):
         up = bucket[i]
@@ -19,6 +21,8 @@ def insertion_sort(bucket):
         bucket[j + 1] = up
     return bucket
 
+# Performs the bucket sort algorithm given an array of jokes
+# Extrapolated from https://stackabuse.com/bucket-sort-in-python/
 def bucket_sort(jokes):
     max_score = max([joke['score'] for joke in jokes])
     bucket_size = max_score + 1
@@ -34,40 +38,50 @@ def bucket_sort(jokes):
 
     return sorted_jokes
 
+# Returns the time it takes to bucket sort the json
+# This function is not used in our final implementation, as we use get_quicksort_time() instead.
+# We run bucket sort on our data and use the result in what is displayed to the user.
 def get_bucketsort_time():
     for item in json_array:
+        # Convert input json file to array
         joke_details = {"title": None, "body": None, "score": None}
         joke_details['title'] = item['title']
         joke_details['body'] = item['body']
         joke_details['score'] = item['score']
         jokes.append(joke_details)
 
+    # Get time taken to perform sort
     t1 = time.perf_counter_ns()
     sorted_jokes = bucket_sort(jokes)
     t2 = time.perf_counter_ns()
 
     return t2 - t1
 
+# Returns the sorted and filtered json with given parameters
 def perform_filtered_bucketsort(num_jokes, min_joke_length, max_joke_length, min_popularity, max_popularity, min_profanity, max_profanity):
     for item in json_array:
+        # Convert input json file to array
         joke_details = {"title": None, "body": None, "score": None}
         joke_details['title'] = item['title']
         joke_details['body'] = item['body']
         joke_details['score'] = item['score']
         jokes.append(joke_details)
 
+    # Get time taken to perform sort
     t1 = time.perf_counter_ns()
     sorted_jokes = bucket_sort(jokes)
     t2 = time.perf_counter_ns()
 
     filtered_jokes = []
     counter = 0
+    # Long chain of ifs that finds a number of jokes in the sorted file that fits the input parameters
     for joke in sorted_jokes:
         if counter != num_jokes:
             if len(joke['title']) + len(joke['body']) >= min_joke_length and len(joke['title']) + len(joke['body']) <= max_joke_length:
                 if joke['body'].lower().strip() not in ("[deleted]", "[removed]"):
                     if math.floor(predict_prob([joke['title'].strip() + joke['body'].strip()])[0] * 100) in range(min_profanity, max_profanity):
                         if joke['score'] >= min_popularity and joke['score'] <= max_popularity:
+                            # We had to implement some custom stop words as the profanity checker missed these sometimes
                             stopwords = ['nsfw', 'NSFW', 'r/jokes', 'reddit', 'Reddit', 'www', 'Edit', 'edit', 'E:', 'EDIT', 'bitch', 'fuck', 'shit', 'pussy', 'blowjob', 'r/', 'erection']
                             stopwordInJoke = False
                             for word in stopwords:
@@ -77,5 +91,6 @@ def perform_filtered_bucketsort(num_jokes, min_joke_length, max_joke_length, min
                                 counter += 1
                                 filtered_jokes.append(joke)
                             
+    # Creates and returns the sorted json of jokes that meet the parameters
     list = [{"num": i + 1, "title": joke['title'].strip(), "body": joke['body'].strip(), "score": joke['score'], "profanity": predict_prob([joke['title'].strip() + joke['body'].strip()])[0]} for i, joke in enumerate(filtered_jokes[:num_jokes])]
     return json.dumps({"bucketsort_time": t2 - t1, "quicksort_time": get_quicksort_time(), "jokes": list}, indent=4)
--- joke-it/back-end/joke_quicksort.py
+++ joke-it/back-end/joke_quicksort.py
@@ -7,9 +7,9 @@ input_file = open('./back-end/dataset/reddit_jokes.json')
 json_array = json.load(input_file)
 jokes = []
 
-#Quick sorts the entire input list by using list comprehension
-#pivot will always be set to the middle item in the list
-#must pass in a parameter to sort by
+# Quick sorts the entire input list by using list comprehension
+# pivot will always be set to the middle item in the list
+# must pass in a parameter to sort by
 def quick_sort(arr, param):
     if len(arr) <= 1:
         return arr
@@ -21,43 +21,50 @@ def quick_sort(arr, param):
 
     return quick_sort(left, param) + middle + quick_sort(right, param)
 
-#Returns the time by quick sorting the json
+# Returns the time it takes to quick sort the json
 def get_quicksort_time():
     for item in json_array:
+        # Convert input json file to array
         joke_details = {"title": None, "body": None, "score": None}
         joke_details['title'] = item['title']
         joke_details['body'] = item['body']
         joke_details['score'] = item['score']
         jokes.append(joke_details)
 
+    # Get time taken to perform sort
     t1 = time.perf_counter_ns()
     sorted_jokes = quick_sort(jokes, "score")
     t2 = time.perf_counter_ns()
 
     return t2 - t1
 
-#Returns the sorted json given a json of jokes and parameters
+# Returns the sorted json given a json of jokes and parameters
+# This function is not used in our final implementation, as we only perform one (visible) sort on the data,
+# but end up using get_quicksort_time() to get the time it takes for quicksort to run
 def perform_filtered_quicksort(num_jokes, min_joke_length, max_joke_length, min_popularity, max_popularity, min_profanity, max_profanity):
     for item in json_array:
+        # Convert input json file to array
         joke_details = {"title": None, "body": None, "score": None}
         joke_details['title'] = item['title']
         joke_details['body'] = item['body']
         joke_details['score'] = item['score']
         jokes.append(joke_details)
 
+    # Get time taken to perform sort
     t1 = time.perf_counter_ns()
     sorted_jokes = quick_sort(jokes, "score")
     t2 = time.perf_counter_ns()
 
     filtered_jokes = []
     counter = 0
-    #Long chain of ifs that finds a number of jokes in the sorted file that fits the input parameters
+    # Long chain of ifs that finds a number of jokes in the sorted file that fits the input parameters
     for joke in sorted_jokes:
         if counter != num_jokes:
             if len(joke['title']) + len(joke['body']) >= min_joke_length and len(joke['title']) + len(joke['body']) <= max_joke_length:
                 if joke['body'].lower().strip() not in ("[deleted]", "[removed]"):
                     if math.floor(predict_prob([joke['title'].strip() + joke['body'].strip()])[0] * 100) in range(min_profanity, max_profanity):
                         if joke['score'] >= min_popularity and joke['score'] <= max_popularity:
+                            # We had to implement some custom stop words as the profanity checker missed these sometimes
                             stopwords = ['nsfw', 'NSFW', 'r/jokes', 'reddit', 'Reddit', 'www', 'Edit', 'edit', 'E:', 'EDIT', 'bitch', 'fuck', 'shit', 'pussy', 'blowjob', 'r/', 'erection']
                             stopwordInJoke = False
                             for word in stopwords:
@@ -66,6 +73,6 @@ def perform_filtered_quicksort(num_jokes, min_joke_length, max_joke_length, min_
                             if joke not in filtered_jokes and not stopwordInJoke:
                                 counter += 1
                                 filtered_jokes.append(joke)
-    #creates and returns the sorted json of jokes that meets the parameters             
+    # Creates and returns the sorted json of jokes that meet the parameters
     list = [{"num": i + 1, "title": joke['title'].strip(), "body": joke['body'].strip(), "score": joke['score'], "profanity": predict_prob([joke['title'].strip() + joke['body'].strip()])[0]} for i, joke in enumerate(filtered_jokes[:num_jokes])]
     return json.dumps({"time": t2 - t1, "jokes": list}, indent=4)
